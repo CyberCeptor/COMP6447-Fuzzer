d3807cb5fc1bf5b5a2769f8cdea676b8e95ef322
Jenson <jenson.morgan@student.unsw.edu.au>
2022-08-05 10:01:30 -0400

    fixing to suit bytes in bytes out and started to add classes

--- mutator_xml.py
+++ mutator_xml.py
@@ -1,57 +1,21 @@
+from gettext import npgettext
 from inspect import findsource
-import subprocess
+from random import sample
 import sys
-import threading
-import numpy as np
 from mutator_base import BaseMutator
-import _thread as thread
 
 # https://docs.python.org/3/library/xml.etree.elementtree.html
 import xml.etree.ElementTree as ET
 
 
-
-
-
-
-
-
-
-
-
-# Test memory with a large XML to check for buffer overflow attacks with tag parsing.
-'''
-In a loop send large xml files to the process and wait for the exit code, and then check the exit code,
-if an error has occured, found a weakness.
-'''
-# Creates a simple xml with the entities being multiplied by the number in the large range.
-def XMLOverflow(program):
+# Returns a xml with tag's multiplied by the number provided. Useful for loop with large values.
+def XMLOverflow(number):
     xmlTemplate="""
     {tag1}
         {input}
     {tag2}
     """
-    for number in range(20000, 50000, 2000):
-        largeXML = xmlTemplate.format(tag1=("<tag>"*number), input=("<input>" * number), tag2=("</tag>" * number))
-        try:
-            p = subprocess.run(program, input=largeXML, timeout=1.0, capture_output=True)
-            if p.returncode != 0:
-                write_out_bad(input)
-        except subprocess.TimeoutExpired:
-            pass
-
-
-
-lock = threading.Lock()
-def write_out_bad(text: bytes):
-    """
-    Write out the failing text. Also acquire a lock and kill the fuzzer to stop
-    it immediately and to prevent multiple writers.
-    """
-    lock.acquire() # never release so that nothing else can possibly write out.
-    with open("bad.txt", "wb") as f:
-        f.write(text)
-    thread.interrupt_main()
+    return xmlTemplate.format(tag1=("<tag>"*number), input=("<input>" * number), tag2=("</tag>" * number))
 
 
 # xml bomb
@@ -64,7 +28,8 @@ exploits xml allowing defining entities.
         20 entityThree's, and so on. If continued until entityEight, the XML parser will unfold
         a single occurrance of entityOne to 1 280 000 000 entityEights (*Gb), causing DOS.
 '''
-def xmlBomb(program):
+# Returns an xmlbomb
+def xmlBomb():
     xmlBomb = """
     <?xml version="1.0"?>
     <!DOCTYPE lolz [
@@ -82,103 +47,111 @@ def xmlBomb(program):
     ]>
     <root>&lolz;</root>
     """
-    try:
-        p = subprocess.run(program, input=xmlBomb, timeout=1.0, capture_output=True)
-        if p.returncode != 0:
-            write_out_bad(input)
-    except subprocess.TimeoutExpired:
-        pass
+
+    return xmlBomb
+
+
 
 
 # XML Root Tag
-''' each xml has exactly one 'root element', enclosing all other elements, thus being the parent (ROOT).
-Removing an ending tag? 
-Change all tags, see what happens
-'''
-# Create a method to find all tags
-'''
-Using xml.etree.ElementTree: API for parsing and creating XML data.
-using fromString to create a list of tags???
-Loop through and set all of the tags to be a pre-determined tag multiplied.
-'''
-
-# https://stackoverflow.com/questions/29596584/getting-a-list-of-xml-tags-in-file-using-xml-etree-elementtree
-def findXMLTags(file):
-    xmlTree = ET.parse(file)
-    tags_list = []
-    for tag in xmlTree.iter():
-        tags_list.append(tag)
-
-    # Remove duplicates to reduce time.
-    # tags_list = list(set(tags_list))
-
-    return tags_list
+''' each xml has exactly one 'root element', enclosing all other elements, thus being the parent (ROOT).'''
 
 
-# https://stackoverflow.com/questions/7465455/how-to-read-root-xml-tag-in-python
-def findRootTag(file):
-    et = ET.parse(file)
-    root = et.getroot()
-    return root
 
 
-def changeRootTag(file):
-    root = findRootTag(file)
-    # print(root.tag)
-    tree = ET.parse(file)
-    for elem in tree.iter():
-        if elem.tag == root.tag:
-            elem.tag = "EEEE"
-
-    # tree.write(file)
 
 
-# https://stackoverflow.com/questions/54796054/xml-change-tag-name-using-python
-# Goes through the file and modifies all of the tags in the file, except for the html tag.
-def changeTags(file, tags_list):
-    tree = ET.parse(file)
-
-    for tag in tags_list:
-        for elem in tree.findall(tag.tag):
-            elem.tag = "Different TAG"
-    
-    # Writes to the file, changing the actual file.
-    tree.write(file)
-    return tree
-
-
-def changeAttributes(file):
-    tree = ET.parse(file)
+# Changes all of the attributes for all tags in the xml
+def changeAttributes(sample_text, attribute_change):
+    tree = ET.fromstring(sample_text)
 
     for element in tree.iter():
-        element.attrib = {"": "%p"}
+        element.attrib = {"": attribute_change}
 
-    # tree.write(file)
-    return tree
+    return ET.tostring(tree)
 
 
-def changeHREFAttribute(file):
-    tree = ET.parse(file)
+
+# Changes the attribute for all 'href' in the xml
+def changeHREFAttribute(sample_text, attribute_change):
+    tree = ET.fromstring(sample_text)
 
     for element in tree.iter():
         if 'href' in element.attrib:
             print(element.attrib)
-            element.attrib['href'] = "EEE"
+            element.attrib['href'] = attribute_change
 
-    # tree.write(file)
-    return tree
+    return ET.tostring(tree)
+
+
+
+
+
+
+
+
+
+
+# class RepeatMutator(BaseMutator):
+#     def get_mutation(self, text: bytes, input: npgettext.ndarray) -> bytes:
+#         return 0
+
+    
+
+
+class TagMutator():
+    # Returns a list of all tags in the xml file.
+    def get_XMLTags(sample_text):
+        xmlTree = ET.fromstring(sample_text)
+        tags_list = []
+        for tag in xmlTree.iter():
+            tags_list.append(tag)
+
+        return tags_list
+
+    #returns the root tag.
+    def get_RootTag(sample_text):
+        tree = ET.ElementTree(ET.fromstring(sample_text))
+        root = tree.getroot()
+        return root
+
+    # Changes the root tag.
+    def changeRootTag(sample_text, root_change):
+        root = TagMutator.get_RootTag(sample_text)
+        tree = ET.fromstring(sample_text)
+        for elem in tree.iter():
+            if elem.tag == root.tag:
+                elem.tag = root_change
+
+        return ET.tostring(tree)
+    
+    # Goes through the file and modifies all of the tags in the file, except for the root (html) tag.
+    def changeTags(sample_text, tag_change):
+        tree = ET.fromstring(sample_text)
+        tags_list = TagMutator.get_XMLTags(sample_text)
+
+        for tag in tags_list:
+            for elem in tree.findall(tag.tag):
+                elem.tag = tag_change
+        
+        return ET.tostring(tree)
+
+
+
+
 
 def main():
 
     file = sys.argv[1]
-    tags_list = findXMLTags(file)
-    print(tags_list)
-    # changeTags(file, tags_list)
-    # root = findRootTag(file)
-    # tree = changeAttributes(file)
-    # print(tree)
-    # changeHREFAttribute(file)
-    # changeRootTag(file)
 
+    with open(file, "rb") as file:
+        sample_text = file.read()
+        # print(TagMutator.changeRootTag(sample_text, "AAA"))
+        # print(TagMutator.changeTags(sample_text, "EEE"))
+        # print(changeAttributes(sample_text, "AAA"))
+        # print(changeHREFAttribute(sample_text, "222"))
+    
+    # print(XMLOverflow(10))
 
-main()
+if __name__ == "__main__":
+    main()
