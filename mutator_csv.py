6325cd8897c42d465c2656142850e003258962c7
Cameron McGowan <z5361406@unsw.edu.au>
2022-08-06 17:39:39 +1000

    RepeatRowMutator implemented, to_csv stub added

---
+++ mutator_csv.py
@@ -0,0 +1,36 @@
+import csv
+import io
+from mutator_base import BaseMutator
+from format_finder import try_json
+
+class CSVRepeatRowMutator(BaseMutator):
+    def get_mutation(self, text: bytes, input: np.ndarray) -> bytes:
+        index = int.from_bytes(input[0].tobytes()[2:4], "little")
+        repeat = int.from_bytes(input[1].tobytes()[2:4], "little")
+        repeat = min(repeat, 10000)
+
+        if not try_csv(text):
+            return text
+            
+        c = list(csv.reader(text))
+        if not index < len(c):
+            return text
+        mutated = c[:index] + ([c[index]] * repeat) + c[index + 1:]
+
+        return to_csv(mutated)
+
+    def get_dimension(self) -> "int":
+        """
+        First element of vector = which row to repeat
+        Second element of vector = how many times to repeat.
+        """
+        return 2
+
+def to_csv(list: mutated_list) -> bytes:
+        #TODO implement this function
+        f = io.StringIO()
+        w = csv.writer(f)
+        w.writerows(mutated)
+        return None
+        
+
