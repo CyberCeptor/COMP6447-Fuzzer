6700e289adcb0e2693e6d73ee6304552b841f311
Cameron McGowan <z5361406@unsw.edu.au>
2022-08-06 18:07:28 +1000

    CSVRepeatColMutator implemented

--- mutator_csv.py
+++ mutator_csv.py
@@ -16,9 +16,8 @@ class CSVRepeatRowMutator(BaseMutator):
         if not index < len(c):
             return text
         mutated = c[:index] + ([c[index]] * repeat) + c[index + 1:]
-
         return to_csv(mutated)
-
+        
     def get_dimension(self) -> "int":
         """
         First element of vector = which row to repeat
@@ -38,7 +37,6 @@ class CSVEmptyRowMutator(BaseMutator):
             return text
         
         mutated = c[:index] + ([""] * len(c[index])) + c[index + 1:]
-
         return to_csv(mutated)
 
     def get_dimension(self) -> "int":
@@ -46,6 +44,30 @@ class CSVEmptyRowMutator(BaseMutator):
         First element of vector = which row to make empty
         """
         return 1
+
+class CSVRepeatColMutator(BaseMutator):
+    def get_mutation(self, text: bytes, input: np.ndarray) -> bytes:
+        index = int.from_bytes(input[0].tobytes()[2:4], "little")
+        repeat = int.from_bytes(input[1].tobytes()[2:4], "little")
+        repeat = min(repeat, 10000)
+
+        if not try_csv(text):
+            return text
+        # Transpose array so we can operate on columns as rows    
+        t = np.transpose(list(csv.reader(text)))
+        
+        if not index < len(c):
+            return text
+        
+        mutated = t[:index] + ([t[index]] * repeat) + t[index + 1:]
+        return to_csv(np.transpose(mutated))
+        
+    def get_dimension(self) -> "int":
+        """
+        First element of vector = which row to repeat
+        Second element of vector = how many times to repeat.
+        """
+        return 2
 
 def to_csv(list: mutated_list) -> bytes:
         #TODO implement this function
